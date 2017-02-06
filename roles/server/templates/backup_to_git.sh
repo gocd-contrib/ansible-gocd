c70ab1cbf5a026400acd9ce8283c1955d53c539d
Neel Radhakrishnan <neel@rdkr.uk>
2017-02-06 12:47:53 +0000

    remove duplicate command

--- roles/server/templates/backup_to_git.sh
+++ roles/server/templates/backup_to_git.sh
@@ -20,8 +20,6 @@ git config push.default simple
 git config user.email "{{ GOCD_ADMIN_EMAIL }}"
 git config user.name "Go Server on {{ ansible_hostname }}"
 
-cp /var/go/go_notify.conf $BACKUP_HOME/go_notify.conf
-
 for f in $BACKUP_HOME/*
 do
    if [ -d "$f" ]
